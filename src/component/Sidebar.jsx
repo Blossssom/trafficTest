8181ee5254d3ec617719965378e4025de1dbc040
blossom <eju2003@gmail.com>
2023-09-06 00:35:52 +0900

    init commit

---
+++ src/component/Sidebar.jsx
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+export default function Sidebar() {
+  return <S_sidebarWrapper>Sidebar</S_sidebarWrapper>;
+}
+
+const S_sidebarWrapper = styled.nav`
+  width: 10%;
+  height: 100%;
+`;
